5e72e994c2a38e75cb1a71d02c3ece0a7d4c1ce5
klso6609 <98013827+klso6609@users.noreply.github.com>
2022-11-25 10:57:00 +0100

    Preprocess fix, new locus report
    Use InfiniumOmni2-5-8v1-5_A1_LocusReport.txt

---
+++ Preprocess_locus_rep_v3.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import random
+
+# Setting precision for the float variables and defining the random seed for choosing a subsample of 1000 SNPs
+pd.set_option("display.precision", 10)
+random.seed(10)
+
+# Defining epsilon
+epsilon = 1*10**-3
+
+# Reading in the Locus Report for Infinium Omni 2-5-8 and extracting chr20, and the appropriate fields
+locus_data = pd.read_csv("InfiniumOmni2-5-8v1-5_A1_LocusReport.txt", sep="\t", index_col=[0], low_memory=False)
+locus_data = locus_data.loc[locus_data['Chr'] == '20']
+locus_data = locus_data.drop(['Name', 'Chr', 'AA Freq', 'AB Freq', 'BB Freq', 'Minor Freq'], axis=1)
+locus_data.loc[locus_data['Call Freq'] > 0.999999, 'Call Freq'] = 1-epsilon
+locus_data_list = list(locus_data['Position'])
+
+# Reading in the positions of the markers in dataset utilized in Clouard et al. 2022
+kgp_chr20 = pd.read_csv("kgp_chr20_pos.txt")
+kgp_chr20_list = list(kgp_chr20.iloc[:, 0])
+
+# Creating a subset of 1000 markers for testing
+index = random.sample(range(1, len(kgp_chr20)), 1000)
+for i in range(1000):
+    kgp_chr20_subset = kgp_chr20.loc[index]
+kgp_chr20_subset_list = list(kgp_chr20_subset.iloc[:, 0])
+
+# Identifying the concordant markers between the Locus Report and Clouard et al. 2022 dataset
+intersecting = pd.Series(list(set(kgp_chr20_list) & set(locus_data_list)))
+
+
+# Identifying the concordant markers between the Locus Report and the 1000 marker subset
+# of the Clouard et al. 2022 dataset
+intersecting_subset = pd.Series(list(set(kgp_chr20_subset_list) & set(locus_data_list)))
+
+print('No of markers in Locus Report: ',len(locus_data_list))
+print('No of markers in Clouard et al. 2022 dataset: ',len(kgp_chr20_list))
+print('No of markers intersecting (Locus Report - Clouard et al. 2022):', len(intersecting))
+print('No of markers intersecting (Locus Report - subset):', len(intersecting_subset))
+
+# Creating dataframe of positions and call frequencies,
+# where markers not found in the locus rapport are given Call Freq = 1 - epsilon
+call_freq_list = [['Pos', 'Call_Freq']]
+for position_kgp in kgp_chr20_list:
+    if position_kgp in intersecting:
+        call_freq_list.append([position_kgp, locus_data.loc[locus_data['Position'] == position_kgp, 'Call Freq']])
+    else:
+        call_freq_list.append([position_kgp, float(1-epsilon)])
+call_freq = pd.DataFrame(call_freq_list[1:], columns=call_freq_list[0])
+
+# Converting dataframe of positions and call frequencies to csv file
+call_freq.to_csv('Call_Freq_Clouard_2022.csv')
